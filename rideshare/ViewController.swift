a3316a4a6f1749ed6ac2cf8a2e7ef695fd9b864c
Nashir Janmohamed <nashirbass@gmail.com>
2020-10-26 23:04:44 -0400

    Create empty xcode project with .gitignore for common Swift files

---
+++ rideshare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rideshare
+//
+//  Created by Nashir Janmohamed on 10/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
