6a17624fc173699903d8dcce3437dc3f80a86b1d
Nashir Janmohamed <nashirbass@gmail.com>
2020-11-11 15:46:58 -0500

    Set up Parse backend in appDelegate

--- rideshare/AppDelegate.swift
+++ rideshare/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = "CNZZcCCXXByWtftzbBeao8OYYsCkhXrcUoTa77eA"
+            $0.clientKey = "U2PG729H3Hy5CpZRp1yNme9hIH8luZwyDNnHUQhQ"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
