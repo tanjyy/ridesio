06a5c8a56b87426c0a1d8fe63d0acb3ed6fd8217
Yao <yyin@college.harvard.edu>
2020-11-27 22:37:07 -0500

    Create New Rides
    (cherry picked from commit d6297e7ab5563ea8b2a37de37fb5325f0f928ac5)

--- rideshare/Controllers/NewRideViewController.swift
+++ rideshare/Controllers/NewRideViewController.swift
@@ -6,8 +6,20 @@
 //
 
 import UIKit
+import AlamofireImage
+import Parse
 
-class NewRideViewController: UIViewController {
+class NewRideViewController: UIViewController,UINavigationControllerDelegate {
+
+    
+
+    @IBOutlet weak var departureLocation: UITextField!
+    
+    @IBOutlet weak var arrivalLocation: UITextField!
+    @IBOutlet weak var departureDatePicker: UIDatePicker!
+    
+    @IBOutlet weak var arrivalDatePicker: UIDatePicker!
+    @IBOutlet weak var rideDetails: UITextField!
 
     
     @IBAction func onDismiss(_ sender: Any) {
@@ -23,6 +35,30 @@ class NewRideViewController: UIViewController {
     @IBAction func onPostRideButton(_ sender: Any) {
         // TODO: add logic to post the ride to Parse and update this user's list of rides
         
+        let ride = PFObject(className:"Rides")
+        ride["departureLocation"] = departureLocation.text
+        ride["arrivalLocation"] = arrivalLocation.text
+
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+
+        let departure_datetime = formatter.string(from: departureDatePicker.date)
+        let arrival_datetime = formatter.string(from: arrivalDatePicker.date)
+        ride["departureDatetime"] = departure_datetime
+        ride["arrivalDatetime"] = arrival_datetime
+        
+        ride["driverId"] = PFUser.current()
+        ride["rideDetails"] = rideDetails.text
+
+        ride.saveInBackground { (success, error)  in
+            if (success) {
+               self.dismiss(animated: true, completion: nil)
+            } else {
+                print("something went wrong")
+            }
+        }
+        
         // TODO: add logic to validate the entries
         let success = true
         if success {
@@ -30,7 +66,7 @@ class NewRideViewController: UIViewController {
             self.dismiss(animated: true, completion: nil)
         } else {
             // TODO: have a popup here that informs the user of the error
-            print("something went wrong")
+           
         }
         
     }
