0670f2deb7597a0ecac437a756b2a8770e04c015
Taha Afzal <tahaafzal5@hotmail.com>
2020-12-06 23:11:32 -0600

    Removed a TODO comment for profile picture, since it is also done.

--- rideshare/Controllers/SettingsViewController.swift
+++ rideshare/Controllers/SettingsViewController.swift
@@ -71,9 +71,7 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
         
         if indexPath.row == 0 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "settingsProfileCell") as! SettingsProfileCell
-            
-            // TODO: update profile picture using profile pic of user
-            
+                        
             cell.name.text = "\(user?["firstName"] as! String) \(user?["lastName"] as! String)"
             
             let imagefile = user!["profilePicture"] as! PFFileObject
