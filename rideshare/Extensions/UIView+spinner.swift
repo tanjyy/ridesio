04256e0c8850e7cacd84b23b2a17385f24e9c55c
Taha Afzal <tahaafzal5@hotmail.com>
2020-12-23 14:56:03 -0600

    Updated names on UIView+spinner.swift

--- rideshare/Extensions/UIView+spinner.swift
+++ rideshare/Extensions/UIView+spinner.swift
@@ -8,27 +8,27 @@
 import Foundation
 import UIKit
 
-var vSpinner : UIView?
+var spinner : UIView?
 
 extension UIViewController {
     func showSpinner(onView : UIView) {
         let spinnerView = UIView.init(frame: onView.bounds)
         spinnerView.backgroundColor = UIColor.init(red: 1, green: 1, blue: 1, alpha: 1)
-        let ai = UIActivityIndicatorView.init(style: .large)
-        ai.startAnimating()
-        ai.center = spinnerView.center
+        let activityIndicator = UIActivityIndicatorView.init(style: .large)
+        activityIndicator.startAnimating()
+        activityIndicator.center = spinnerView.center
         DispatchQueue.main.async {
-            spinnerView.addSubview(ai)
+            spinnerView.addSubview(activityIndicator)
             onView.addSubview(spinnerView)
         }
         
-        vSpinner = spinnerView
+        spinner = spinnerView
     }
     
     func removeSpinner() {
         DispatchQueue.main.async {
-            vSpinner?.removeFromSuperview()
-            vSpinner = nil
+            spinner?.removeFromSuperview()
+            spinner = nil
         }
     }
 }
